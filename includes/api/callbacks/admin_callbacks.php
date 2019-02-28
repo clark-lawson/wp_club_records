72d3a2329a9d90fb8dce84806831bb2ba2b1928b
Clark Lawson <thesasgeek@gmail.com>
2019-02-28 16:15:01 +0000

    Admin Custom Fields Added

---
+++ includes/api/callbacks/admin_callbacks.php
@@ -0,0 +1,36 @@
+<?php 
+/**
+ * @package  wp_club_records
+ */
+
+require_once plugin_dir_path( dirname( __FILE__, 3 ) )  . 'includes/base/base_controller.php';
+
+/**
+* 
+*/
+class AdminCallbacks extends BaseController {
+
+    public function adminDashboard() {
+        return require_once ("$this->plugin_path/includes/templates/admin.php");
+    }
+
+    public function ClubRecordsOptGroup( $input) {
+        return $input;
+    }
+
+    public function ClubRecordsAdminSection( ) {
+        echo 'Check this section';
+    }
+
+    public function ClubRecordsTextExample( ) {
+
+        $value = esc_attr( get_option( 'text_example' ));
+        echo '<input type="text" 
+               class="regular-text" 
+               name="text_example" 
+               value="' . $value . '" 
+               placeholder="Write Something Here!" />';
+    }
+
+
+}
--- includes/templates/admin.php
+++ includes/templates/admin.php
@@ -1,1 +1,12 @@
-<H1>Club Records Plugin</H1>
+<div class="wrap">
+  <H1>Club Records Plugin Required</H1>
+  <?php settings_errors(); ?>
+
+  <form method="post" action="options.php">
+    <?php
+      settings_fields('club_records_opt_group' );
+      do_settings_sections( 'club_records' );
+      submit_button();
+    ?>
+  </form>
+</div>
