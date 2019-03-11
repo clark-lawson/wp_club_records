4ff2371eed7898dd1ccb1d6c716f6839a8ebce5f
clark-lawson <thesasgeek@gmail.com>
2019-03-11 13:58:05 +0000

    Modular Plugin Sections

--- includes/api/callbacks/manager_callbacks.php
+++ includes/api/callbacks/manager_callbacks.php
@@ -33,8 +33,9 @@ class ManagerCallbacks extends BaseController {
         $name = $args['label_for'];
 		$classes = $args['class'];
 		$option_name = $args['option_name'];
-		$checkbox = get_option( $option_name );
-		echo '<div class="' . $classes . '"><input type="checkbox" id="' . $name . '" name="' . $option_name . '[' . $name . ']" value="1" class="" ' . ($checkbox[$name] ? 'checked' : '') . '><label for="' . $name . '"><div></div></label></div>';
+        $checkbox = get_option( $option_name );
+        $checked = isset($checkbox[$name]) ? ($checkbox[$name] ? true : false) : false;
+		echo '<div class="' . $classes . '"><input type="checkbox" id="' . $name . '" name="' . $option_name . '[' . $name . ']" value="1" class="" ' . ( $checked ? 'checked' : '') . '><label for="' . $name . '"><div></div></label></div>';
     }
 
 }
--- includes/init.php
+++ includes/init.php
@@ -38,6 +38,25 @@ final class Init extends BaseController
           $settingslnkservice->register();
        }
 
+       require_once plugin_dir_path( dirname( __FILE__, 1 ) )  . 'includes/base/road_records_controller.php';
+       $RoadRecordsController = new RoadRecordsController();
+       if ( method_exists( $RoadRecordsController, 'register' ) ) {
+          $RoadRecordsController->register();
+       }
+
+       require_once plugin_dir_path( dirname( __FILE__, 1 ) )  . 'includes/base/track_records_controller.php';
+       $TrackRecordsController = new TrackRecordsController();
+       if ( method_exists( $TrackRecordsController, 'register' ) ) {
+          $TrackRecordsController->register();
+       }
+
+       require_once plugin_dir_path( dirname( __FILE__, 1 ) )  . 'includes/base/field_records_controller.php';
+       $FieldRecordsController = new FieldRecordsController();
+       if ( method_exists( $FieldRecordsController, 'register' ) ) {
+          $FieldRecordsController->register();
+       }
+
+
     }
 
 }
